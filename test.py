4d60618599cbe3d30bbccd337e6024053ddefedb
Ian <ian.delrazo@swyti.com.mx>
2023-09-28 09:42:11 -0600

    funcionamiento primer modulo

--- test.py
+++ test.py
@@ -1,21 +1,29 @@
 import time
+import os
+import pywhatkit as pykit
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-driver.get("https://www.google.com")
+# driver = webdriver.Chrome()
+# driver.get("https://web.whatsapp.com/")
+# driver.maximize_window()
 
-# Abrir una nueva pestaña
-driver.execute_script("window.open('https://www.google.com/cucaracha')")
-driver.switch_to.window(driver.window_handles[1])
-# assert "Python" in driver.title
 
-time.sleep(5)
-""" elem = driver.find_element(By.NAME, "q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source """
-driver.close()
+# pykit.sendwhatmsg_instantly("+525518999585", "HI",10)
+
+pykit.sendwhats_image("+5215618999585","lobo.jpg")
+
+# mensajes = ["buenas","estoy probando","un bot", "y tu eres", "el sujeto", "de pruebas"]
+"""
+mensajes
+     for i in mensajes:
+    pykit.sendwhatmsg_instantly("+5215618999585", i,10, True)
+    time.sleep(2) """
+""" pykit.sendwhatmsg_instantly("+5215618999585", "HI XD",10, True,2)
+pykit.sendwhatmsg("+5215618999585", "Hi", 12,37, 10, True, 3) """
+
+time.sleep(10)
+
+# driver.close()
---
+++ whats2.py
@@ -0,0 +1,20 @@
+import pywhatkit as kit
+import time
+
+# kit.sendwhats_image("+5215618999585","lobo.jpg")
+
+# mensajes = ["buenas","estoy probando","un bot", "y tu eres", "el sujeto", "de pruebas"]
+"""
+mensajes
+     for i in mensajes:
+    pykit.sendwhatmsg_instantly("+5215618999585", i,10, True)
+    time.sleep(2) """
+#enrique
+kit.sendwhatmsg_instantly("+5215527552381", "Hola",10, True,1)
+time.sleep(4)
+#omar
+kit.sendwhatmsg("+5215591958770", "Hola", 12, 12, 10, True, 1)
+
+time.sleep(4)
+
+kit.sendwhatmsg_instantly("15854968266","que día es hoy",10,True)
